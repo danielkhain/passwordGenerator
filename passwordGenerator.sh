ebed38e6a053bd51fd8ae48598579af9b32e0721
danielkhain <44067988+danielkhain@users.noreply.github.com>
2020-09-10 02:40:18 -0400

    Updated comments

--- passwordGenerator.sh
+++ passwordGenerator.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Creates special characters array
 array[0]="!"
 array[1]="@"
 array[2]="#"
@@ -7,7 +8,9 @@ array[3]="$"
 array[4]="%"
 array[5]="&"
 
+
 size=${#array[@]}
 index=$(($RANDOM % $size))
-#gshuf /usr/share/dict/words -n2 | python3 -c "import sys; print(sys.stdin.read().title(), end='')" | { tr -d '\n'; echo -n; }; echo -n $RANDOM; echo ${array[$index]}
+
+#Outputs the password
 gshuf /usr/share/dict/words -n2 | python3 -c "import sys; print(sys.stdin.read().title(), end='')" | { tr -d '\n'; echo -n; }; gshuf -i 0-2000 -n1 | { tr -d '\n'; echo -n; }; echo ${array[$index]}
